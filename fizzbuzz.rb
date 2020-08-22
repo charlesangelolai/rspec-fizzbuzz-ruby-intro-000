1fbc1716847bbcd66657538394e3dac0bf707fa3
Charles Lai <charlesangeloplai@gmail.com>
2020-08-22 22:05:13 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,6 +3,13 @@
 require_relative './spec_helper.rb'
 require_relative '../fizzbuzz.rb'
 
-def fizzbuzz
-
+def fizzbuzz(int)
+  if int % 3 == 0
+    return "Fizz"
+  elsif int % 5 == 0
+    return "Buzz"
+  elsif (int % 5 ==  0) && (int % 3 == 0)
+    return "FizzBuzz"
+  else
+    return nil
 end
